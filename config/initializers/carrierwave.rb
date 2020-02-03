ee6bcad19d2b8d2989bc7415d29d9e89e3f1aca3
Nissy24-AAA <aaanuuhasi0914takauno@docomo.ne.jp>
2020-02-03 19:13:37 +0900

    トップページ実装

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'furima67a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima67a'
 end
