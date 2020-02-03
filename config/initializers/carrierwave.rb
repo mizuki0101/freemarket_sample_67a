ba08f35ed52e553ad6f32c4ebcff6cc85cf6d19e
Nissy24-AAA <aaanuuhasi0914takauno@docomo.ne.jp>
2020-02-03 15:15:53 +0900

    トップページ実装

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.production?
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -14,4 +15,7 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'furima67a'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima67a'
+  else
+    config.storage = :file
+  end  
 end
