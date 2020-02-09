1eb78c874f2dde4b6cd0f78218c431718a17f302
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-02-09 11:51:28 +0900

    productテーブルmigrationファイル編集
    productsテーブルのbuyer_idは商品を購入した時につくものなのでnull:falseにしていると商品出品できないため修正。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates :images, presence: true
 end
--- db/migrate/20200209023841_create_products.rb
+++ db/migrate/20200209023841_create_products.rb
@@ -1,7 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string :name, null: false, index: true
+      t.string :size, null: false
+      t.string :bland_id
+      t.text :description, null: false
+      t.integer :price
+      t.references :delivery_date, null: false, foreign_key: true
+      t.references :status, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+      t.references :saler_id, null: false, foreign_key: true
+      t.references :buyer_id, foreign_key: true
       t.timestamps
     end
   end
