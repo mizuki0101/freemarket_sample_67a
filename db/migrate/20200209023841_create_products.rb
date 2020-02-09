67a8c661b56aca3e457f989893133d7cf9f32e24
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-02-09 12:10:25 +0900

    Update 20200209023841_create_products.rb

--- db/migrate/20200209023841_create_products.rb
+++ db/migrate/20200209023841_create_products.rb
@@ -2,10 +2,10 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name, null: false, index: true
-      t.string :size, null: false
+      t.string :size
       t.string :bland_id
       t.text :description, null: false
-      t.integer :price
+      t.integer :price, null: false
       t.integer :prefecture_id, null: false
       t.references :delivery_date, null: false, foreign_key: true
       t.references :prefecture, null: false, foreign_key: true
