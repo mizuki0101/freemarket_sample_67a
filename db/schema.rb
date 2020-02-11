369efed3757f69a5696e2b5caa50d740c4b6de3d
Nissy24-AAA <aaanuuhasi0914takauno@docomo.ne.jp>
2020-02-11 13:30:40 +0900

    ログイン

--- db/schema.rb
+++ db/schema.rb
@@ -13,22 +13,16 @@
 ActiveRecord::Schema.define(version: 2020_02_09_074006) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postnumber", null: false
+    t.integer "postnumber", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.integer "house_number", null: false
     t.string "building"
-    t.string "send_family_name", null: false
-    t.string "send_first_name", null: false
-    t.string "send_family_name_kana", null: false
-    t.string "send_first_name_kana", null: false
-    t.integer "phonenumber"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,13 +33,18 @@ ActiveRecord::Schema.define(version: 2020_02_09_074006) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
+  create_table "sells", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "phonenumber", null: false
+    t.integer "phonenumber", null: false
     t.integer "birth_year", null: false
     t.integer "birth_month", null: false
     t.integer "birth_date", null: false
