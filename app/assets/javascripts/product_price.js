6339b318afe3ba6b0e9c8183210a28f96eadac10
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-02-12 20:40:05 +0900

    console.log 削除しました。

--- app/assets/javascripts/product_price.js
+++ app/assets/javascripts/product_price.js
@@ -1,11 +1,8 @@
 $(document).on('turbolinks:load', ()=> {
   $('#product_price').on('keyup', function() {
     let getPrice = $(this).val();
-    console.log(getPrice);
     let commission = getPrice / 10;
     let profit = getPrice - commission;
-    console.log(commission);
-    console.log(profit);
     $('p#commision').text(commission);
     $('p#profit').text(profit);
     if($.type(getPrice) != "number"){
