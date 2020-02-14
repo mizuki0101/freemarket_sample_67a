8726ce73b0a52e594218db61f3e3988895cc7c81
Nissy24-AAA <aaanuuhasi0914takauno@docomo.ne.jp>
2020-02-14 18:21:45 +0900

    トップページ、商品詳細、商品削除のサーバーサイド　訂正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'products#index'
-  resources :products, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :products do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
