1156c0a5cd228c9de08f09420080c4e767ce33c7
mizuki0101 <gresky11@gmail.com>
2020-02-14 21:49:27 +0900

    修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :images, dependent: :destroy, index_errors: true
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :user
+  belongs_to :user, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivarydate
