fd526939b93bdc451b608d3ebdfaf10eb953e9f7
kimurasou <cb.102444@gmail.com>
2020-02-18 13:49:40 +0900

    住所編集

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -26,16 +26,27 @@ class MypageController < ApplicationController
   def addressconfirmation
   end
 
-  # def edit
-  #   @user = User.find(params[:id])
-  # end
 
-  # def update
-  #   @address = Address.find(params[:id])
-  #   if @address.update(address_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+
+  def edit
+    @user = User.find(params[:id])
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    # binding.pry
+    if @address.update(address_params)
+      redirect_to mypage_index_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:send_family_name, :send_first_name, :send_family_name_kana, :send_first_name_kana, :postnumber, :prefecture_id, :city, :house_number, :building, :phonenumber)
+  end
 end
